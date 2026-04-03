f3d47d894297efe700659c030dfb9706eece4eb2
Akshay S Dinesh <akshay@learnlearn.in>
2026-04-03 17:32:49 +0530

    fix: strange issue with geekblog partial rendering a stray inverted comma pair

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 mkdir -p themes/geekblog
 curl -L https://github.com/thegeeklab/hugo-geekblog/releases/latest/download/hugo-geekblog.tar.gz | tar -xz -C themes/geekblog/ --strip-components=1
+# Fixing strange issue with featured block returning a stray set of inverted commas
+sed -i.bak '/{{- with partial "utils\/featured" . }}/,/{{- end }}/d' themes/geekblog/layouts/partials/microformats/schema.html
